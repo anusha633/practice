6f7226227f2dee7ecb262dd04c7ead1cf3940ba8
anusha633 <palavali.rani@ltimindtree.com>
2024-11-08 23:29:15 +0530

    Update and rename anusha.yml to bash.sh

--- .github/workflows/bash.sh
+++ .github/workflows/bash.sh
@@ -1,13 +1,17 @@
 name: Bash Script
-on: push
+on:
+  push
+
 jobs:
   bash-script:
     runs-on: ubuntu-latest
 
     steps:
+      - name: Checkout code
+        uses: actions/checkout@v2
+
       - name: Run Bash script
         run: bash bash.sh
-
          
             
              
